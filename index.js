161afdf212a078a7e75f24e023fd8c4fe6e7c23b
Toushik <tayebhossain018@gmail.com>
2023-05-20 04:19:12 +0600

    applied post and get

--- index.js
+++ index.js
@@ -1,48 +1,76 @@
 const express = require('express');
 const cors = require('cors');
-const { MongoClient, ServerApiVersion } = require('mongodb');
-require('dotenv').config()
+const { MongoClient, ServerApiVersion, ObjectId } = require('mongodb');
+require('dotenv').config();
+
 const app = express();
 const port = process.env.PORT || 5000;
 
-// middleware
-
-app.use(cors())
+app.use(cors());
 app.use(express.json());
 
-// console.log(process.env.DB_PASS)
-
-
 const uri = `mongodb+srv://${process.env.DB_USER}:${process.env.DB_PASS}@cluster0.qesst1e.mongodb.net/?retryWrites=true&w=majority`;
-
-// Create a MongoClient with a MongoClientOptions object to set the Stable API version
 const client = new MongoClient(uri, {
-  serverApi: {
-    version: ServerApiVersion.v1,
-    strict: true,
-    deprecationErrors: true,
-  }
+    serverApi: {
+        version: ServerApiVersion.v1,
+        strict: true,
+        deprecationErrors: true,
+    }
 });
 
 async function run() {
-  try {
-    // Connect the client to the server	(optional starting in v4.7)
-    await client.connect();
-    // Send a ping to confirm a successful connection
-    await client.db("admin").command({ ping: 1 });
-    console.log("Pinged your deployment. You successfully connected to MongoDB!");
-  } finally {
-    // Ensures that the client will close when you finish/error
-    await client.close();
-  }
+    try {
+        await client.connect();
+
+        const toysCollection = client.db('toyJoy').collection('toys');
+
+        app.post('/addtoys', async (req, res) => {
+            const toy = req.body;
+
+            const result = await toysCollection.insertOne(toy);
+            res.send(result);
+
+        });
+
+        app.get('/alltoys', async (req, res) => {
+            try {
+              const result = await toysCollection.find({}).limit(20).toArray();
+              res.send(result);
+            } catch (error) {
+              console.log(error);
+              res.status(500).json({ message: "Failed to retrieve toys from the database" });
+            }
+          });
+
+
+
+          app.get('/toy/:id', async (req, res) => {
+            const id = req.params.id;
+      
+            try {
+              const toy = await toysCollection.findOne({ _id: new ObjectId(id) });
+              res.send(toy);
+            } catch (error) {
+              console.log(error);
+              res.status(500).json({ message: "Failed to retrieve toy from the database" });
+            }
+          });
+          
+          
+
+        await client.db("admin").command({ ping: 1 });
+        console.log("Pinged your deployment. You successfully connected to MongoDB!");
+    } finally {
+        // await client.close();
+    }
 }
+
 run().catch(console.dir);
 
-
 app.get('/', (req, res) => {
-    res.send('Toy server is running.....')
-})
+    res.send('Toy server is running.....');
+});
 
 app.listen(port, () => {
     console.log(`Toy server is running on port ${port}`);
-})
+});
