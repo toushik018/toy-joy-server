2d966b526e169108a0736eeba1257dd6b47b8046
Toushik <tayebhossain018@gmail.com>
2023-05-21 19:09:58 +0600

    added the updated operation

--- index.js
+++ index.js
@@ -68,6 +68,13 @@ async function run() {
 
     //Update operation
     
+    app.get('/mytoys/:id', async( req, res) => {
+      const id = req.params.id;
+      const query = {_id: new ObjectId(id)}
+
+      const result = await toysCollection.findOne(query);
+      res.send(result)
+    })
 
     // delete
 
