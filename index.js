b6730ebb22e2ba1d64eb7d7578a091d1e6ad2d46
Toushik <tayebhossain018@gmail.com>
2023-05-21 18:48:04 +0600

    fix some errors

--- index.js
+++ index.js
@@ -81,9 +81,6 @@ async function run() {
 
 
 
-
-
-
     await client.db("admin").command({ ping: 1 });
     console.log("Pinged your deployment. You successfully connected to MongoDB!");
   } finally {
